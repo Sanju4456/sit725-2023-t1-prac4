0bca86dfab1e2aff8eb03051cd25f54c1081c062
Sanju  Jayawickrama <82253527+Sanju4456@users.noreply.github.com>
2024-04-08 16:44:58 +1000

    Add files via upload

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const path = require('path');
+const { addCards, connectToDatabase, disconnectToDatabase } = require("./monogodb");
+
+const app = express();
+const router = express.Router();
+const port = 3000;
+
+app.use(express.static(__dirname + '/'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+
+router.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+router.post("/", async function(req, res) {
+  try {
+    await addCards(req.body.first_name, req.body.last_name, req.body.email, req.body.password);
+    res.status(201).send("Data inserted successfully");
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("An error occurred while inserting data");
+  }
+});
+app.get('/api/FormDetails', (req, res) => {
+  const inputData = {
+    message: 'This is the input data you requested',
+    data: req.query // Assuming input data is passed as query parameters
+  };
+  res.json(inputData);
+});
+
+app.use('/', router);
+
+app.listen(port, async () => {
+  await connectToDatabase();
+  console.log(`server on port ${port}`);
+});
